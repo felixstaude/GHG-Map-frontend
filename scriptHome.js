c4f21a2d7ff769720bc35e067b29d8a45558284d
bxrbeq <lelousta04@gmail.com>
2024-06-25 16:23:46 +0200

    added styling of homepage

--- scriptHome.js
+++ scriptHome.js
@@ -1,27 +1,84 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', async function() {
     let map = document.getElementById('linkMap');
     let login = document.getElementById('linkLogin');
     let search = document.getElementById('linkSearch');
+
+    let mapEl = document.getElementById('mapEl');
+    let loginEl = document.getElementById('loginEl');
+    let searchEl = document.getElementById('searchEl');
+
     let width = window.innerWidth;
     let factor = width / 3.5;
     
+    let accessToken = localStorage.getItem('accessToken');
+
+    if (accessToken) {
+        const data = await validateToken(accessToken);
+        loginEl.textContent = `Hallo ${data.login}`;
+    }
+    //set size of backgound for nav elements
     map.style.backgroundSize = `${factor}%`;
     login.style.backgroundSize = `${factor}%`;
     search.style.backgroundSize = `${factor}%`;
     setBGOffset(map,login,search);
 
+    //  --  hover effect for navigation buttons
 
-    let mapEl = document.getElementById('mapEl');
-    let loginEl = document.getElementById('loginEl');
-    let searchEl = document.getElementById('searchEl');
-    map.addEventListener('mouseover', (e) => (userHover(e,mapEl,'#fff', 'transparent')))
-    login.addEventListener('mouseover', (e) => (userHover(e,loginEl,'#fff', 'transparent')))
-    search.addEventListener('mouseover', (e) => (userHover(e,searchEl,'#fff', 'transparent')))
+
+    //make nav white on hover
+    map.addEventListener('mouseover', (e) => (navHover(e,mapEl,'#fff', 'transparent')))
+    login.addEventListener('mouseover', (e) => (navHover(e,loginEl,'#fff', 'transparent')))
+    search.addEventListener('mouseover', (e) => (navHover(e,searchEl,'#fff', 'transparent')))
     
-    map.addEventListener('mouseout', (e) => (userHover(e,mapEl,'transparent','#fff')))
-    login.addEventListener('mouseout', (e) => (userHover(e,loginEl,'transparent','#fff')))
-    search.addEventListener('mouseout', (e) => (userHover(e,searchEl,'transparent','#fff')))
+    //make nav bg back to transparent/bg-image 
+    map.addEventListener('mouseout', (e) => (navHover(e,mapEl,'transparent','#fff')))
+    login.addEventListener('mouseout', (e) => (navHover(e,loginEl,'transparent','#fff')))
+    search.addEventListener('mouseout', (e) => (navHover(e,searchEl,'transparent','#fff')))
+
+    //move footer when user has mouse
+    let move = document.getElementById('footerMove');
+
+    setFooter();
+
+    if (matchMedia('(pointer:fine)').matches) {
+        window.addEventListener('wheel', (e) => {
+            if (e.deltaY > 0) { // user scrolling down when delta is positive
+                move.classList.add('footerTasty') //show footer
+            } else if (e.deltaY < 0) {
+                move.classList.remove('footerTasty') // hide footer -- thanks to angetange for the class names 😄
+            }
+        })
+    } else {
+        let start;
+        let length;
+        window.addEventListener('touchstart', (e) => {
+            start = e.changedTouches[0].clientY;
+        });
+        window.addEventListener('touchend', (e) => {
+            length = start - e.changedTouches[0].clientY;
+            if (length > 20) {
+                move.classList.add('footerTasty');
+            } else if (length < 0) {
+                move.classList.remove('footerTasty');
+            }
+        });
+    }
+
+    // set position for popups to make them stick to "Link"
+    setPosition('bxrbeq');setPosition('felix');setPosition('charlie');
+
+    // show popups on hover
 });
+
+function setPosition(x) {
+    let move = document.getElementById('footerMove');
+    let link = document.getElementById(`${x}Link`);
+    let posX = link.offsetWidth / 2 - 50; // margin to left
+    let posY = link.offsetHeight; // margin to bottom from top edge
+    let popup = document.getElementById(`${x}Popup`);
+    popup.style.left = posX + 'px';
+    popup.style.bottom = posY + 'px';
+}
 
 window.addEventListener('resize', function(){
     let map = document.getElementById('linkMap');
@@ -30,20 +87,43 @@ window.addEventListener('resize', function(){
     let width = window.innerWidth;
     let factor = width / 3.5;
     
+    //set size of backgound for nav elements 
     map.style.backgroundSize = `${factor}%`;
     login.style.backgroundSize = `${factor}%`;
     search.style.backgroundSize = `${factor}%`;
     setBGOffset(map,login,search);
+    setFooter();
 });
 
-function setBGOffset(a,b,c) {
-    a.style.backgroundPositionX = `-${a.offsetLeft}px`;
-    // a.style.backgroundPositionY = `${a.offsetTop}px`; irgendwie auch mit Höhe vom Bild verrechnen, damit es an das gecropte Bild angepasst werden kann
-    b.style.backgroundPositionX = `-${b.offsetLeft}px`;
-    c.style.backgroundPositionX = `-${c.offsetLeft}px`;
+function setFooter() {
+    let footer = document.getElementsByTagName('footer')[0];
+    let move = document.getElementById('footerMove');
+    let moveHeight = move.offsetHeight;
+
+    move.style.marginTop = `${moveHeight}px`;
+    footer.style.height = moveHeight + 'px';
 }
 
-async function userHover(a,b,c1,c2) {
+function setBGOffset(a,b,c) {
+    let mw =  document.getElementsByTagName('main')[0].offsetWidth;
+    let mh = document.getElementsByTagName('main')[0].offsetHeight;
+
+    // top margin
+    let aTop = (mw / 2 - mh / 2) + a.offsetTop;
+    let bTop = (mw / 2 - mh / 2) + b.offsetTop;
+    let cTop = (mw / 2 - mh / 2) + c.offsetTop;
+
+    a.style.backgroundPositionX = `-${a.offsetLeft}px`; // set "left margin"
+    a.style.backgroundPositionY = `-${aTop}px`; // set "top magin"
+
+    b.style.backgroundPositionX = `-${b.offsetLeft}px`;
+    b.style.backgroundPositionY = `-${bTop}px`;
+
+    c.style.backgroundPositionX = `-${c.offsetLeft}px`;
+    c.style.backgroundPositionY = `-${cTop}px`;
+}
+
+async function navHover(a,b,c1,c2) {
     let progress = 0;
 
     while (progress < 100) {
@@ -52,7 +132,8 @@ async function userHover(a,b,c1,c2) {
         await sleep(1);
     };
 }
-
 const sleep = (ms) => {
     return new Promise(resolve => setTimeout((resolve), ms));
 }
+
+// bin neidisch 🥹 (kontext: felix hat kiz album und merch)

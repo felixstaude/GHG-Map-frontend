221c0c44cd6d5a5c127e6264af623ccfad9df581
bxrbeq <lelousta04@gmail.com>
2024-08-10 19:09:26 +0200

    fixed

--- admin/adminScript.js
+++ admin/adminScript.js
@@ -3,8 +3,7 @@ document.addEventListener('DOMContentLoaded', async () =>{
     //get pins
 
     async function createLists(x) {
-        const response = await fetch(`http://localhost:8080/api/pin/admin/${x}roved.json`, {
-        //const response = await fetch(`http://localhost:8080/api/pin/admin/all/${x}roved/userId=${localStorage.userId}`, {
+        const response = await fetch(`http://localhost:8080/api/pin/admin/all/${x}roved/userId=${localStorage.userId}`, {
                 method: 'GET',
             headers: {'access_token': localStorage.accessToken}
         })
